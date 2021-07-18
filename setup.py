8d2ea9b07b90c7135ede35a88890bd50c182d4c5
NO ONE <38581702+HACKER097@users.noreply.github.com>
2021-07-18 17:31:49 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pygame'],  # Optional
+    install_requires=['pygame', 'pydub', 'pytube'],  # Optional
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
