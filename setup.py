51bfb1897d952885598de28c13c0264a7f67cc32
NO ONE <38581702+HACKER097@users.noreply.github.com>
2021-08-29 19:46:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,13 +30,13 @@ setup(
     # https://packaging.python.org/specifications/core-metadata/#name
     name='wisdom-tree',  # Required
 
-    # Versions should comply with PEP 440:
+    # Versions should comply with PE 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.20',  # Required
+    version='0.1.0',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
