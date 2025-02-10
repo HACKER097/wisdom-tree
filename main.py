a5935d3b1ffba844b98f2d087adc640d7664d318
Shubham Soni <shubxam@gmail.com>
2025-02-10 16:36:21 +0530

    add main menus for pomodoro and media

--- main.py
+++ main.py
@@ -139,211 +139,6 @@ def adjust_media_volume(tree1: Any, new_volume: int, maxx: int) -> None:
     tree1.invert = True
     tree1.isnotify = True
 
-def key_events(stdscr: Any, tree1: Any, maxx: int) -> None:
-
-    global EFFECT_VOLUME  # now imported from config.py
-
-    key = stdscr.getch()
-
-    if key in (curses.KEY_UP, ord("k")):
-        tree1.showtimer = True
-        tree1.selectedtimer -= 1
-        tree1.timerhidetime = int(time.time()) + 5
-
-    if key in (curses.KEY_DOWN, ord("j")):
-        tree1.showtimer = True
-        tree1.selectedtimer += 1
-        tree1.timerhidetime = int(time.time()) + 5
-
-    if key == curses.KEY_ENTER or key == 10 or key == 13:  # this is enter key
-        if tree1.showtimer:
-            if tree1.currentmenu == "timer":
-                tree1.starttimer(tree1.selectedtimer, stdscr, maxx)
-            else:
-                tree1.featureselect(tree1.selectedtimer, maxx, stdscr)
-            play_sound(TIMER_START_SOUND)
-            tree1.showtimer = False
-
-        if tree1.breakover:
-            tree1.breakover = False
-            tree1.starttimer(tree1.selectedtimer, stdscr, maxx)
-            play_sound(TIMER_START_SOUND)
-
-    if key == ord("q"):
-        with open(RES_FOLDER / "treedata", "wb") as treedata:
-            pickle.dump(tree1.age, treedata, protocol=None)
-        exit()
-
-    if key == ord("u"):
-        toggle_sounds()
-
-    if key in (curses.KEY_RIGHT, ord("l")):
-        if tree1.showtimer:
-            tree1.selectedtimer = 0
-            tree1.currentmenu = "feature"
-
-        else:
-            tree1.radiomode = False
-            tree1.music_list_num += 1
-            if tree1.music_list_num > len(tree1.music_list) - 1:
-                tree1.music_list_num = len(tree1.music_list) - 1
-            tree1.media.stop()
-            tree1.media = vlc.MediaPlayer(tree1.music_list[tree1.music_list_num])
-            tree1.media.play()
-            if os.name == "posix":
-                tree1.notifystring = (
-                    "Playing: "
-                    + str(tree1.music_list[tree1.music_list_num]).split("/")[-1]
-                )
-            else:
-                tree1.notifystring = (
-                    "Playing: "
-                    + str(tree1.music_list[tree1.music_list_num]).split('\\')[-1]
-                )
-
-            tree1.notifyendtime = int(time.time()) + 5
-            tree1.isnotify = True
-            
-    if key in (curses.KEY_LEFT, ord("h")):
-        if tree1.showtimer:
-            tree1.selectedtimer = 0
-            tree1.currentmenu = "timer"
-        else:
-            tree1.radiomode = False
-            tree1.music_list_num -= 1
-            if tree1.music_list_num < 0:
-                tree1.music_list_num = 0
-            tree1.media.stop()
-            tree1.media = vlc.MediaPlayer(tree1.music_list[tree1.music_list_num])
-            tree1.media.play()
-            if os.name == "posix":
-                tree1.notifystring = (
-                    "Playing: "
-                    + str(tree1.music_list[tree1.music_list_num]).split("/")[-1]
-                )
-            else:
-                tree1.notifystring = (
-                    "Playing: "
-                    + str(tree1.music_list[tree1.music_list_num]).split('\\')[-1]
-                )
-
-            tree1.notifyendtime = int(time.time()) + 5
-            tree1.isnotify = True
-
-    if key == ord(" "):
-        if tree1.media.is_playing():
-            tree1.media.pause()
-        tree1.pause = True
-        tree1.pausetime = time.time()
-
-    if key == ord("m"):
-        tree1.media.pause()
-
-    if key == ord("]"):
-        new_volume = tree1.media.audio_get_volume() + 1
-        adjust_media_volume(tree1, new_volume, maxx)
-        
-    if key == ord("["):
-        new_volume = tree1.media.audio_get_volume() - 1
-        adjust_media_volume(tree1, new_volume, maxx)
-
-        
-    if key == ord("}"):
-        EFFECT_VOLUME = min(100, EFFECT_VOLUME+1)
-
-        tree1.notifyendtime = int(time.time()) + 2
-
-        volume = str(EFFECT_VOLUME) + "%"
-        tree1.notifystring = " "*round(maxx*(EFFECT_VOLUME/100)-len(volume)-2) + volume
-        tree1.invert = True
-        tree1.isnotify = True
-
-    if key == ord("{"):
-        EFFECT_VOLUME = max(0, EFFECT_VOLUME-1)
-
-        tree1.notifyendtime = int(time.time()) + 2
-
-        volume = str(EFFECT_VOLUME) + "%"
-        tree1.notifystring = " "*round(maxx*(EFFECT_VOLUME/100)-len(volume)-2) + volume
-        tree1.invert = True
-        tree1.isnotify = True
-
-    if key == ord("="):
-
-        if tree1.media.get_time()+10000 < tree1.media.get_length():
-            tree1.media.set_time(i_time=tree1.media.get_time()+10000)
-        else:
-            tree1.media.set_time(tree1.media.get_length()-1)
-
-        time_sec = tree1.media.get_time()/1000
-        display_time =  str(int(time_sec / 60)).zfill(2) + ":" + str(int(time_sec) % 60).zfill(2)
-        tree1.notifyendtime = int(time.time()) + 2
-        try:
-            tree1.notifystring = " "*(round(maxx*(tree1.media.get_time()/tree1.media.get_length()))-len(display_time)) + display_time
-        except ZeroDivisionError:
-            pass
-        tree1.invert = True
-        tree1.isnotify = True
-
-    if key == ord("-"):
-        if tree1.media.get_time()-10000 > 0:
-            tree1.media.set_time(i_time=tree1.media.get_time()-10000)
-        else:
-            tree1.media.set_time(0)
-
-
-        time_sec = tree1.media.get_time()/1000
-        display_time =  str(int(time_sec / 60)).zfill(2) + ":" + str(int(time_sec) % 60).zfill(2)
-        tree1.notifyendtime = int(time.time()) + 2
-        tree1.notifystring = " "*(round(maxx*(tree1.media.get_time()/tree1.media.get_length()))-len(display_time)) + display_time
-        tree1.invert = True
-        tree1.isnotify = True
-
-    if key == ord("r"):
-        if tree1.isloop:
-            tree1.isloop = False
-        else:
-            tree1.isloop = True
-
-        tree1.notifyendtime = int(time.time()) + 2
-        tree1.notifystring = "REPEAT: " + str(tree1.isloop)
-        tree1.invert = False
-        tree1.isnotify = True
-        
-    # if key == ord("/"):
-    #     # change the quote
-    #     quote = getqt()
-    #     addtext(int(maxx / 2), int(maxy * 5 / 6), quote, anilen, stdscr, 2)
-
-    for i in range(10):
-        if key == ord(str(i)):
-            tree1.media.set_time(i_time=int(tree1.media.get_length()*(i/10)))
-
-            time_sec = tree1.media.get_time()/1000
-            display_time =  str(int(time_sec / 60)).zfill(2) + ":" + str(int(time_sec) % 60).zfill(2)
-            tree1.notifyendtime = int(time.time()) + 2
-            tree1.notifystring = " "*(round(maxx*(tree1.media.get_time()/tree1.media.get_length()))-len(display_time)) + display_time
-            tree1.invert = True
-            tree1.isnotify = True
-
-
-
-def GetSong(link):
-
-    video = YouTube("http://youtube.com/" + link.split("/")[-1] )
-
-    try:
-        video.streams
-    except:
-        return "WRONG LINK ERROR"
-
-    try:
-        songfile = str(video.streams.get_by_itag(251).download(timeout=30))
-    except:
-        return "DOWNLOAD ERROR"
-
-    return songfile
-
 def GetLinks(search_string):
     search_url = "https://www.youtube.com/results?search_query=" + search_string.replace(" ", "+")
     # html = urllib.request.urlopen()
@@ -351,8 +146,118 @@ def GetLinks(search_string):
     video_ids = re.findall(r"watch\?v=(\S{11})", html.content.decode())
     return "http://youtube.com/watch?v=" + str(video_ids[0])
 
-#print(GetSong(GetLinks(input())))
-#time.sleep(100)
+def key_events(stdscr: Any, tree1: Any, maxx: int) -> None:
+    global EFFECT_VOLUME  # Moved global declaration to the top.
+    key = stdscr.getch()
+    # Update menu activity timestamp on navigation keys
+    if key in (curses.KEY_UP, ord("k"), curses.KEY_DOWN, ord("j"), curses.KEY_RIGHT, ord("l"), curses.KEY_LEFT, ord("h"), 27):
+        tree1.menu_last_active = time.time()
+
+    # New menu navigation logic
+    if key in (curses.KEY_UP, ord("k")):
+        if tree1.show_sub_menu:
+            tree1.current_sub_index = (tree1.current_sub_index - 1) % len(tree1.sub_menus[tree1.main_menu[tree1.current_main_index]])
+        else:
+            tree1.current_main_index = (tree1.current_main_index - 1) % len(tree1.main_menu)
+    elif key in (curses.KEY_DOWN, ord("j")):
+        if tree1.show_sub_menu:
+            tree1.current_sub_index = (tree1.current_sub_index + 1) % len(tree1.sub_menus[tree1.main_menu[tree1.current_main_index]])
+        else:
+            tree1.current_main_index = (tree1.current_main_index + 1) % len(tree1.main_menu)
+    elif key in (curses.KEY_RIGHT, ord("l")):
+        if not tree1.show_sub_menu:
+            tree1.show_sub_menu = True
+            tree1.current_sub_index = 0
+    elif key in (curses.KEY_LEFT, ord("h"), 27):  # Left arrow or Escape key
+        tree1.show_sub_menu = False
+    elif key == curses.KEY_ENTER or key in [10, 13]:
+        if tree1.show_sub_menu:
+            current_main = tree1.main_menu[tree1.current_main_index]
+            if current_main == "POMODORO TIMER":
+                tree1.breakendtext = "BREAK IS OVER, PRESS ENTER TO START NEW TIMER"
+                tree1.starttimer(tree1.current_sub_index, stdscr, maxx)
+            elif current_main == "MEDIA":
+                tree1.featureselect(tree1.current_sub_index, maxx, stdscr)
+            play_sound(TIMER_START_SOUND)
+            tree1.show_sub_menu = False
+        # If no sub menu, ignore enter
+    # Existing controls for quitting, toggling sounds, media controls, etc.
+    if key == ord("q"):
+        with open(RES_FOLDER / "treedata", "wb") as treedata:
+            pickle.dump(tree1.age, treedata, protocol=None)
+        exit()
+    if key == ord("u"):
+        toggle_sounds()
+    if key == ord(" "):
+        if tree1.media.is_playing():
+            tree1.media.pause()
+        tree1.pause = True
+        tree1.pausetime = time.time()
+    if key == ord("m"):
+        tree1.media.pause()
+    if key == ord("]"):
+        new_volume = tree1.media.audio_get_volume() + 1
+        adjust_media_volume(tree1, new_volume, maxx)
+    if key == ord("["):
+        new_volume = tree1.media.audio_get_volume() - 1
+        adjust_media_volume(tree1, new_volume, maxx)
+    if key == ord("}"):
+        EFFECT_VOLUME = min(100, EFFECT_VOLUME + 1)
+        tree1.notifyendtime = int(time.time()) + 2
+        volume = str(EFFECT_VOLUME) + "%"
+        tree1.notifystring = " " * round(maxx * (EFFECT_VOLUME / 100) - len(volume) - 2) + volume
+        tree1.invert = True
+        tree1.isnotify = True
+    if key == ord("{"):
+        EFFECT_VOLUME = max(0, EFFECT_VOLUME - 1)
+        tree1.notifyendtime = int(time.time()) + 2
+        volume = str(EFFECT_VOLUME) + "%"
+        tree1.notifystring = " " * round(maxx * (EFFECT_VOLUME / 100) - len(volume) - 2) + volume
+        tree1.invert = True
+        tree1.isnotify = True
+    if key == ord("="):
+        if tree1.media.get_time() + 10000 < tree1.media.get_length():
+            tree1.media.set_time(i_time=tree1.media.get_time() + 10000)
+        else:
+            tree1.media.set_time(tree1.media.get_length() - 1)
+        time_sec = tree1.media.get_time() / 1000
+        display_time = str(int(time_sec / 60)).zfill(2) + ":" + str(int(time_sec) % 60)
+        tree1.notifyendtime = int(time.time()) + 2
+        try:
+            tree1.notifystring = " " * (round(maxx * (tree1.media.get_time() / tree1.media.get_length())) - len(display_time)) + display_time
+        except ZeroDivisionError:
+            pass
+        tree1.invert = True
+        tree1.isnotify = True
+    if key == ord("-"):
+        if tree1.media.get_time() - 10000 > 0:
+            tree1.media.set_time(i_time=tree1.media.get_time() - 10000)
+        else:
+            tree1.media.set_time(0)
+        time_sec = tree1.media.get_time() / 1000
+        display_time = str(int(time_sec / 60)).zfill(2) + ":" + str(int(time_sec) % 60)
+        tree1.notifyendtime = int(time.time()) + 2
+        tree1.notifystring = " " * (round(maxx * (tree1.media.get_time() / tree1.media.get_length())) - len(display_time)) + display_time
+        tree1.invert = True
+        tree1.isnotify = True
+    if key == ord("r"):
+        if tree1.isloop:
+            tree1.isloop = False
+        else:
+            tree1.isloop = True
+        tree1.notifyendtime = int(time.time()) + 2
+        tree1.notifystring = "REPEAT: " + str(tree1.isloop)
+        tree1.invert = False
+        tree1.isnotify = True
+    for i in range(10):
+        if key == ord(str(i)):
+            tree1.media.set_time(i_time=int(tree1.media.get_length() * (i / 10)))
+            time_sec = tree1.media.get_time() / 1000
+            display_time = str(int(time_sec / 60)).zfill(2) + ":" + str(int(time_sec) % 60)
+            tree1.notifyendtime = int(time.time()) + 2
+            tree1.notifystring = " " * (round(maxx * (tree1.media.get_time() / tree1.media.get_length())) - len(display_time)) + display_time
+            tree1.invert = True
+            tree1.isnotify = True
 
 class tree:
     def __init__(self, stdscr, age):
@@ -400,6 +305,26 @@ class tree:
         self.invert = False
         self.breakendtext = "BREAK IS OVER, PRESS ENTER TO START NEW TIMER"
         self.isloop = False
+        # Remove old timerlist and featurelist; add new main menu and sub menus
+        self.main_menu = ["POMODORO TIMER", "MEDIA"]
+        self.sub_menus = {
+            "POMODORO TIMER": [
+                " POMODORO {}+{} ".format(TIMER_WORK_MINS[0], TIMER_BREAK_MINS[0]),
+                " POMODORO {}+{} ".format(TIMER_WORK_MINS[1], TIMER_BREAK_MINS[1]),
+                " POMODORO {}+{} ".format(TIMER_WORK_MINS[2], TIMER_BREAK_MINS[2]),
+                " POMODORO {}+{} ".format(TIMER_WORK_MINS[3], TIMER_BREAK_MINS[3]),
+                " CUSTOM TIMER ",
+                " END TIMER NOW ",
+            ],
+            "MEDIA": [
+                " PLAY MUSIC FROM YOUTUBE ",
+                " ~CONCENTRATION MUSIC "
+            ]
+        }
+        self.current_main_index = 0
+        self.current_sub_index = 0
+        self.show_sub_menu = False
+        self.menu_last_active = time.time()  # used for fading menus
 
     def display(self, maxx, maxy, seconds):
         '''draw the bonsai tree on stdscr'''
@@ -431,8 +356,11 @@ class tree:
             self.stdscr,
             3,
         )
-
-        # RAIN
+        # ADDED: show timer countdown if active
+        if self.istimer:
+            remaining = max(0, int(self.workendtime - time.time()))
+            timer_text = "WORK: {:02d}:{:02d}".format(remaining // 60, remaining % 60)
+            self.stdscr.addstr(int(maxy * 3 / 4) + 2, int(maxx/2 - len(timer_text)//2), timer_text, curses.color_pair(2))
 
     def rain(self, maxx, maxy, seconds, intensity, speed, char, color_pair):
         random.seed(
@@ -474,75 +402,22 @@ class tree:
             #self.invert = False
 
     def menudisplay(self, stdscr, maxy, maxx):
-        if self.showtimer:
-
-            if self.currentmenu == "timer":
-                if self.selectedtimer > len(self.timerlist) - 1:
-                    self.selectedtimer = len(self.timerlist) - 1
-                if self.selectedtimer < 0:
-                    self.selectedtimer = 0
-
-            if self.currentmenu == "feature":
-                if self.selectedtimer > len(self.featurelist) - 1:
-                    self.selectedtimer = len(self.featurelist) - 1
-                if self.selectedtimer < 0:
-                    self.selectedtimer = 0
-
-
-            for i in range(len(self.timerlist)):
-                if i == self.selectedtimer and self.currentmenu == "timer":
-                    stdscr.addstr(
-                        int((maxy - len(self.timerlist)*2) / 2) + i * 2,
-                        int(maxx / 25 + 4),
-                        self.timerlist[i],
-                        curses.A_REVERSE,
-                    )
-                else:
-                    stdscr.addstr(
-                        int((maxy - len(self.timerlist)*2) / 2) + i * 2,
-                        int(maxx / 25),
-                        self.timerlist[i],
-                    )
-
-            for i in range(len(self.featurelist)):
-                if i == self.selectedtimer and self.currentmenu == "feature":
-                    stdscr.addstr(
-                        int((maxy - len(self.featurelist)*2) / 2) + i * 2,
-                        int(maxx * 24 / 25 - len(self.featurelist[i])) - 4,
-                        self.featurelist[i],
-                        curses.A_REVERSE,
-                    )
-                else:
-                    stdscr.addstr(
-                        int((maxy - len(self.featurelist)*2) / 2) + i * 2,
-                        int(maxx * 24 / 25 - len(self.featurelist[i])),
-                        self.featurelist[i],
-                    )
-
-        if int(time.time()) >= self.timerhidetime:
-            self.showtimer = False
-
-        if self.istimer:
-            self.secondsleft = int(self.workendtime) - int(time.time())
-            timertext = (
-                "Break in: "
-                + str(int(self.secondsleft / 60)).zfill(2)
-                + ":"
-                + str(self.secondsleft % 60).zfill(2)
-            )
-            stdscr.addstr(
-                int(maxy * 10 / 11), int(maxx / 2 - len(timertext) / 2), timertext
-            )
-
-        if self.breakover:
-            self.stdscr.addstr(
-                int(maxy * 10 / 11),
-                int(
-                    maxx / 2 - len(self.breakendtext) / 2
-                ),
-                self.breakendtext,
-                curses.A_BLINK | curses.A_BOLD,
-            )
+        # Show menus only if active within last 5 seconds (fading effect)
+        if time.time() - self.menu_last_active < 5:
+            # Draw main menu on left
+            start_y = int(maxy / 2) - len(self.main_menu)
+            for idx, item in enumerate(self.main_menu):
+                style = curses.A_REVERSE if idx == self.current_main_index and not self.show_sub_menu else curses.A_NORMAL
+                stdscr.addstr(start_y + idx * 2, 2, item, style)
+            # Draw sub menu on the right if active
+            if self.show_sub_menu:
+                submenu_items = self.sub_menus[self.main_menu[self.current_main_index]]
+                submenu_start_y = start_y  # align submenus vertically with main menus
+                max_item_len = max(len(item) for item in submenu_items)
+                submenu_x = maxx - max_item_len - 2  # position so that long text doesn't spill over
+                for idx, item in enumerate(submenu_items):
+                    style = curses.A_REVERSE if idx == self.current_sub_index else curses.A_NORMAL
+                    stdscr.addstr(submenu_start_y + idx * 2, submenu_x, item, style)
 
     def breakstart(self):
         if self.istimer:
@@ -580,7 +455,7 @@ class tree:
             self.breakendtext = "TIMER IS OVER, PRESS ENTER"
             self.worktime = 0
             self.breaktime = 0
-            self.istimer == False
+            self.istimer = False   # Changed from '==' to '='
         
         elif inputtime == 4:
 
